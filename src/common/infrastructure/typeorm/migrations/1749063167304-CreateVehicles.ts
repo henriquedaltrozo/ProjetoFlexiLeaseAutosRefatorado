7c9cd94cff1c92712f102a1856e2e4064a55f8e8
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-06-04 18:34:57 -0300

    feat: create migration for vehicles and accessories tables

---
+++ src/common/infrastructure/typeorm/migrations/1749063167304-CreateVehicles.ts
@@ -0,0 +1,95 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm'
+
+export class CreateVehicles1749063167304 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'vehicles',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'model',
+            type: 'varchar',
+          },
+          {
+            name: 'color',
+            type: 'varchar',
+          },
+          {
+            name: 'year',
+            type: 'int',
+          },
+          {
+            name: 'value_per_day',
+            type: 'numeric',
+          },
+          {
+            name: 'number_of_passengers',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    )
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'accessories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'vehicle_id',
+            type: 'uuid',
+          },
+        ],
+      }),
+    )
+
+    await queryRunner.createForeignKey(
+      'accessories',
+      new TableForeignKey({
+        columnNames: ['vehicle_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'vehicles',
+        onDelete: 'CASCADE',
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('accessories')
+    await queryRunner.dropTable('vehicles')
+  }
+}
