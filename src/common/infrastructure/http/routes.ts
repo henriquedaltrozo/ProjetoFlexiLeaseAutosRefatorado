1aff2239262e6051d305fdcf547064977320a8a6
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-06-11 12:02:16 -0300

    feat: add vehicles routing

--- src/common/infrastructure/http/routes.ts
+++ src/common/infrastructure/http/routes.ts
@@ -1,3 +1,4 @@
+import { vehiclesRouter } from '@/vehicles/infrastructure/http/routes/vehicles.route'
 import { Router } from 'express'
 
 const routes = Router()
@@ -5,5 +6,7 @@ const routes = Router()
 routes.get('/', (req, res) => {
   return res.status(200).json({ message: 'Olá Dev!' })
 })
+
+routes.use('/vehicles', vehiclesRouter)
 
 export { routes }
