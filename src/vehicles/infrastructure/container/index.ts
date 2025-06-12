cb95a00c6cf678d230c646316d5eaeab49b21143
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-06-11 21:53:52 -0300

    feat: implement get vehicle use case

--- src/vehicles/infrastructure/container/index.ts
+++ src/vehicles/infrastructure/container/index.ts
@@ -1,10 +1,12 @@
 import { dataSource } from '@/common/infrastructure/typeorm'
 import { CreateVehicleUseCase } from '@/vehicles/application/usecases/create-vehicle.usecase'
+import { GetVehicleUseCase } from '@/vehicles/application/usecases/get-vehicle.usecase'
 import { Vehicle } from '@/vehicles/infrastructure/typeorm/entities/vehicles.entity'
 import { VehiclesTypeOrmRepository } from '@/vehicles/infrastructure/typeorm/repositories/vehicles-typeorm.repository'
 import { container } from 'tsyringe'
 
 container.registerSingleton('VehicleRepository', VehiclesTypeOrmRepository)
+
 container.registerSingleton(
   'CreateVehicleUseCase',
   CreateVehicleUseCase.UseCase,
@@ -14,3 +16,5 @@ container.registerInstance(
   'VehiclesDefaultTypeormRepository',
   dataSource.getRepository(Vehicle),
 )
+
+container.registerSingleton('GetVehicleUseCase', GetVehicleUseCase.UseCase)
