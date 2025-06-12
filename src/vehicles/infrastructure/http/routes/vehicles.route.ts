32b0790a48063ae875021d68592990e7a90f84f6
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-06-11 22:19:40 -0300

    feat: add vehicle get route

--- src/vehicles/infrastructure/http/routes/vehicles.route.ts
+++ src/vehicles/infrastructure/http/routes/vehicles.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { createVehicleController } from '../controllers/create-vehicle.controller'
+import { getVehicleController } from '../controllers/get-vehicle.controller'
 
 const vehiclesRouter = Router()
 
 vehiclesRouter.post('/', createVehicleController)
+vehiclesRouter.get('/:id', getVehicleController)
 
 export { vehiclesRouter }
