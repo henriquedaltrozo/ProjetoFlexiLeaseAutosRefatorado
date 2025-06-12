d1235b900b3e428c82dd6ac72ebd81829820ad0e
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-06-12 00:37:27 -0300

    feat: add delete vehicle route

--- src/vehicles/infrastructure/http/routes/vehicles.route.ts
+++ src/vehicles/infrastructure/http/routes/vehicles.route.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { createVehicleController } from '../controllers/create-vehicle.controller'
 import { getVehicleController } from '../controllers/get-vehicle.controller'
 import { updateVehicleController } from '../controllers/update-vehicle.controller'
+import { deleteVehicleController } from '../controllers/delete-vehicle.controller'
 
 const vehiclesRouter = Router()
 
 vehiclesRouter.post('/', createVehicleController)
 vehiclesRouter.get('/:id', getVehicleController)
 vehiclesRouter.put('/:id', updateVehicleController)
+vehiclesRouter.delete('/:id', deleteVehicleController)
 
 export { vehiclesRouter }
