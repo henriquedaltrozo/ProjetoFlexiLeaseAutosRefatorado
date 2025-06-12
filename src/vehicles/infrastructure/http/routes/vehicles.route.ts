57a7acac36f8461ba5acafda933623c8765d4a92
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-06-12 00:17:34 -0300

    feat: add update vehicle route

--- src/vehicles/infrastructure/http/routes/vehicles.route.ts
+++ src/vehicles/infrastructure/http/routes/vehicles.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { createVehicleController } from '../controllers/create-vehicle.controller'
 import { getVehicleController } from '../controllers/get-vehicle.controller'
+import { updateVehicleController } from '../controllers/update-vehicle.controller'
 
 const vehiclesRouter = Router()
 
 vehiclesRouter.post('/', createVehicleController)
 vehiclesRouter.get('/:id', getVehicleController)
+vehiclesRouter.put('/:id', updateVehicleController)
 
 export { vehiclesRouter }
