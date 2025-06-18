30644d9cebf512c4a14507317a879084ad25b2d4
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-06-18 14:23:19 -0300

    feat: add route for search users

--- src/users/infrastructure/http/routes/users.route.ts
+++ src/users/infrastructure/http/routes/users.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { createUserController } from '../controllers/create-user.controller'
+import { searchUserController } from '../controllers/search-user.controller'
 
 const usersRouter = Router()
 
 usersRouter.post('/', createUserController)
+usersRouter.get('/', searchUserController)
 
 export { usersRouter }
