357bf277f6bd278ef6a43f7e321db446da1218aa
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-07-15 17:56:31 -0300

    feat: add get user route to users router

--- src/users/infrastructure/http/routes/users.route.ts
+++ src/users/infrastructure/http/routes/users.route.ts
@@ -3,6 +3,7 @@ import { createUserController } from '../controllers/create-user.controller'
 import { searchUserController } from '../controllers/search-user.controller'
 import { updateUserController } from '../controllers/update-user.controller'
 import { deleteUserController } from '../controllers/delete-user.controller'
+import { getUserController } from '../controllers/get-user.controller'
 import { isAuthenticated } from '@/common/infrastructure/http/middlewares/isAuthenticated'
 
 const usersRouter = Router()
@@ -10,6 +11,7 @@ const usersRouter = Router()
 usersRouter.post('/', createUserController)
 usersRouter.use(isAuthenticated)
 usersRouter.get('/', searchUserController)
+usersRouter.get('/:id', getUserController)
 usersRouter.put('/:id', updateUserController)
 usersRouter.delete('/:id', deleteUserController)
 
