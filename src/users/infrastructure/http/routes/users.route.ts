d523114aa45d3a6d0d6cbc5c89ee679169c1539b
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-07-15 17:43:40 -0300

    feat: add update and delete user routes to users router

--- src/users/infrastructure/http/routes/users.route.ts
+++ src/users/infrastructure/http/routes/users.route.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { createUserController } from '../controllers/create-user.controller'
 import { searchUserController } from '../controllers/search-user.controller'
+import { updateUserController } from '../controllers/update-user.controller'
+import { deleteUserController } from '../controllers/delete-user.controller'
 import { isAuthenticated } from '@/common/infrastructure/http/middlewares/isAuthenticated'
 
 const usersRouter = Router()
@@ -8,5 +10,7 @@ const usersRouter = Router()
 usersRouter.post('/', createUserController)
 usersRouter.use(isAuthenticated)
 usersRouter.get('/', searchUserController)
+usersRouter.put('/:id', updateUserController)
+usersRouter.delete('/:id', deleteUserController)
 
 export { usersRouter }
