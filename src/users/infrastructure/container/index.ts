bba4040fe9b15403f5fe7304bbee6c1190bee595
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-07-15 17:43:09 -0300

    feat: register update user and delete user use cases in the container

--- src/users/infrastructure/container/index.ts
+++ src/users/infrastructure/container/index.ts
@@ -5,6 +5,8 @@ import { User } from '../typeorm/entities/users.entity'
 import { CreateUserUseCase } from '@/users/application/usecases/create-user.usecase'
 import { SearchUserUseCase } from '@/users/application/usecases/search-user.usecase'
 import { AuthenticateUserUseCase } from '@/users/application/usecases/authenticate-user.usecase'
+import { UpdateUserUseCase } from '@/users/application/usecases/update-user.usecase'
+import { DeleteUserUseCase } from '@/users/application/usecases/delete-user.usecase'
 
 container.registerSingleton('UsersRepository', UsersTypeormRepository)
 
@@ -21,3 +23,7 @@ container.registerSingleton(
   'AuthenticateUserUseCase',
   AuthenticateUserUseCase.UseCase,
 )
+
+container.registerSingleton('UpdateUserUseCase', UpdateUserUseCase.UseCase)
+
+container.registerSingleton('DeleteUserUseCase', DeleteUserUseCase.UseCase)
