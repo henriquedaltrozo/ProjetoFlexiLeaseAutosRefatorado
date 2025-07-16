8de026c519f215177a49000f59a95495b77788f9
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-07-16 20:17:02 -0300

    feat: add swagger documentation for authentication route

--- src/users/infrastructure/http/routes/auth.route.ts
+++ src/users/infrastructure/http/routes/auth.route.ts
@@ -1,6 +1,46 @@
 import { Router } from 'express'
 import { authenticateUserController } from '../controllers/authenticate-user.controller'
 
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: Autenticação de usuários
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Fazer login do usuário
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginInput'
+ *     responses:
+ *       200:
+ *         description: Login realizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/LoginResponse'
+ *       400:
+ *         description: Dados inválidos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       401:
+ *         description: Credenciais inválidas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+
 const authRouter = Router()
 
 authRouter.post('/login', authenticateUserController)
